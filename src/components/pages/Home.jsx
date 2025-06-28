d3c27d3fae9c3611a0ab712ae319b15e898974aa
tituvai <titu17940@gmail.com>
2025-06-28 13:34:58 +0600

    first commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RootLayout from '../layouts/rootLayout'
+
+
+const Home = () => {
+  return (
+    <>
+        <RootLayout/>
+    </>
+  )
+}
+
+export default Home
